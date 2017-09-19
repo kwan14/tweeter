d05fad35e94adf0a9a0d33607be11c85fb5b292e
kwan14 <mendel.kwan@telus.net>
2017-09-19 23:27:36 +0000

    Added letter counter on compose tweet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,17 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(function () {
+  $(".new-tweet textarea").on("keyup", function (event) {
+    const MAXLENGTH = 140;
+    let count = charCounter($(this).val());
+    if (count <= MAXLENGTH) {
+      $(".new-tweet span").text(String(count));
+    } else {
+      let overCount = count - MAXLENGTH;
+      $(this).closest(".new-tweet").find(".counter").text(("-" + overCount)).css("color", "red");
+    }
+  });
+
+});
 
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,4 @@
+
+function charCounter (string) {
+  return(string.length);
+}
